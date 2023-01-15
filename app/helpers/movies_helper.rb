74e899408b64851702bc55c35be9203dfc35c422
Sumeet Haryani <sumeetharyani3@gmail.com>
2023-01-15 11:37:41 +0000

    Add view helper methods

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,14 @@
 module MoviesHelper
+  def total_gross(movie)
+    if movie.flop?
+      return "Flop!"
+    else
+      number_to_currency(movie.total_gross, precision: 0)
+    end
+  end
+
+    def year_of(movie)
+    movie.release_on.strftime("%Y")
+  end
+
 end
